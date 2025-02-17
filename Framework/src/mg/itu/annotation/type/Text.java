5e96acab0aa2cb6318aba513acc9896d5450154b
Ony Herilaza <onyherilaza27@gmail.com>
2025-02-17 10:00:00 +0300

    sprint17-2541

---
+++ Framework/src/mg/itu/annotation/type/Text.java
@@ -0,0 +1,14 @@
+package mg.itu.annotation.type;
+
+import java.lang.annotation.ElementType;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Text {
+    int longueurMax() default 255;
+    String errorMessage() default "Le texte depasse la limite autorisée : ";
+}
